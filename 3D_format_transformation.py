21504c9baf1aaaa1e666ea76cd1e7ec2d69d0f28
chiellini <18319277690@163.com>
2023-02-27 15:49:21 +0800

    add 3d format trasformation script to npy (foreground, background, membrane)

--- 3D_format_transformation.py
+++ 3D_format_transformation.py
@@ -4,6 +4,8 @@ import glob
 import os
 import numpy as np
 
+from scipy.ndimage.morphology import binary_closing
+
 # ------user's packages------
 from utils.utils import save_indexed_tif,scale2index,nib_save
 
@@ -31,9 +33,60 @@ def nifti2tiff(root,target,segmented):
         save_file_path = os.path.join(target,  embryo_name+'_'+tp+ ".tif")
         save_indexed_tif(save_file_path, nifti_file_arr,segmented=segmented)
 
+def nift2npy(root,target):
+    nifti_file_paths=glob.glob(os.path.join(root,'*.nii.gz'))
+    for nifti_file_path in nifti_file_paths:
+        nifti_file_arr=nibabel.load(nifti_file_path).get_fdata()
+        embryo_name = os.path.basename(nifti_file_path).split(".")[0].split('_')[0]
+        tp = os.path.basename(nifti_file_path).split(".")[0].split('_')[1]
+        save_file_path = os.path.join(target, embryo_name + '_' + tp + ".npy")
+        np.save(save_file_path, nifti_file_arr)
+def nift2npy_3type(root,target):
+    nifti_file_paths=glob.glob(os.path.join(root,'SegCell/*.nii.gz'))
+    for nifti_file_path in nifti_file_paths:
+        nifti_file_arr=nibabel.load(nifti_file_path).get_fdata()
+        embryo_name = os.path.basename(nifti_file_path).split(".")[0].split('_')[0]
+        tp = os.path.basename(nifti_file_path).split(".")[0].split('_')[1]
+        # -------------
+        save_file_path_1 = os.path.join(target, 'masks',embryo_name + '_' + tp + "_foreground.npy")
+        foreground_arr=nifti_file_arr.copy()
+        foreground_arr[nifti_file_arr!=0]=1
+        print('foreground',np.unique(foreground_arr,return_counts=True))
+        np.save(save_file_path_1, foreground_arr)
+        # -----------------
+        save_file_path_2 = os.path.join(target, 'masks', embryo_name + '_' + tp + "_background.npy")
+        background_arr=np.ones(nifti_file_arr.shape)
+        binary_closing_arr_back=binary_closing(nifti_file_arr != 0,iterations=5)
+        background_arr[binary_closing_arr_back]=0
+        np.save(save_file_path_2, background_arr)
+        print('backgaround',np.unique(background_arr,return_counts=True))
+        # ------------------
+        save_file_path_3 = os.path.join(target, 'masks', embryo_name + '_' + tp + "_membrane.npy")
+        nifti_file_path_membrane=os.path.join(root,'SegMemb',embryo_name + '_' + tp +'_segMemb.nii.gz')
+        nifti_file_arr_membrane = nibabel.load(nifti_file_path_membrane).get_fdata()
+        np.save(save_file_path_3,nifti_file_arr_membrane)
+        print('membrane',np.unique(nifti_file_arr_membrane,return_counts=True))
+
+
+
+
+
+
 
 if __name__ == "__main__":
     print('3d format transforming')
-    tiff2nifti(r'F:\test',r'F:\test')
+    # tiff2nifti(r'F:\test',r'F:\test')
     # nifti2tiff(r'C:\Users\zelinli6\Downloads\stardist_3d_data\testnifti\images',r'C:\Users\zelinli6\Downloads\stardist_3d_data\test\images',segmented=False)
     # nifti2tiff(r'C:\Users\zelinli6\Downloads\stardist_3d_data\trainnifti\masks',r'C:\Users\zelinli6\Downloads\stardist_3d_data\train\masks',segmented=True)
+
+    # 3dcellseg dataset preparation
+    # nift2npy(r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\MembraneProjectData\DataSource\170614plc1p1\RawMemb',r'C:\Users\zelinli6\Downloads\3dcellseg_data\trainnpy\images')
+    # nift2npy(r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\MembraneProjectData\DataSource\170704plc1p1\RawMemb',r'C:\Users\zelinli6\Downloads\3dcellseg_data\trainnpy\images')
+    nift2npy(r'D:\MembraneProjectData\DataSource\170614plc1p1\SegCell', r'F:\3dcellseg_data\trainnpy\masks')
+    nift2npy(r'D:\MembraneProjectData\DataSource\170704plc1p1\SegCell', r'F:\3dcellseg_data\trainnpy\masks')
+
+    # nift2npy_3type(r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\MembraneProjectData\DataSource\170704plc1p1',r'C:\Users\zelinli6\Downloads\3dcellseg_data\trainnpy')
+    # nift2npy_3type(r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\MembraneProjectData\DataSource\170614plc1p1',r'C:\Users\zelinli6\Downloads\3dcellseg_data\trainnpy')
+
+
+    nift2npy(r'C:\Users\zelinli6\Downloads\3dcellseg_data\testnifti\images', r'F:\3dcellseg_data\testnpy\images')
