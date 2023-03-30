eac776ddeb2c1018cb849b8aac91caa2dfbc99d6
chiellini <18319277690@163.com>
2023-03-30 09:51:25 +0800

    fixed a bug on facet indices and rename the mtl name as well

--- seperate_process.py
+++ seperate_process.py
@@ -23,20 +23,70 @@ def resize_the_segcell_niigz():
             print(name_tp,seg_array.shape,'_->>>',resize_seg_array.shape)
 
 
+def set_up_new_training_and_evaluation_data():
+    # =================================================
+    #  Set new training evaluation dataset
+    # =================================================
 
+    # root_path=r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\MembraneProjectData\CMapEvaluationData\CShaperRawLabelData'
+    # target_path=r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\MembraneProjectData\CMapEvaluationData\CShaperRawLabelData\tem'
+    # niigz_paths=glob.glob(os.path.join(root_path,'*.nii.gz'))
+    # for niigz in niigz_paths:
+    #     arraythis=nib.load(niigz).get_fdata()
+    #     # nib_save(os.path.join(target_path,os.path.basename(niigz)),array)
+    #     # arraythis=np.flip(arraythis,0)
+    #     # arraythis=np.flip(arraythis,1)
+    #     arraythis=np.flip(arraythis,2)
+    #     img_stack = resize(image=arraythis, output_shape=(205,285,134), preserve_range=True, order=1).astype(np.uint8)
+
+    #     nib_save(os.path.join(target_path,os.path.basename(niigz)),img_stack)
+    # -----------generate seg memb-----------------------------------------
+    # trainin_seg_folder = r"F:\TrainingandEvaluation\evaluation\SegCell"
+    # traingi_memb_dst_folder = r"F:\TrainingandEvaluation\evaluation\SegMemb"
+    # # embryo_name = "170704plc1p1"
+    #
+    # seg_cell_file_paths = glob.glob(os.path.join(trainin_seg_folder,"*.nii.gz"))
+    #
+    # for seg_file_path in seg_cell_file_paths:
+    #     seg = nib.load(seg_file_path).get_fdata()
+    #     memb = get_boundary(seg).astype(np.uint8)
+    #     embryo_name,tp=os.path.basename(seg_file_path).split('.')[0].split('_')[:2]
+    #     # save_name_cell = os.path.join(dst_folder, "{}_{}_segCell.nii.gz".format(embryo_name, tp))
+    #     save_seg_memb_path = os.path.join(traingi_memb_dst_folder, "{}_{}_segMemb.nii.gz".format(embryo_name, tp))
+    #     # nib_save(save_name_cell, seg)
+    #     nib_save(save_seg_memb_path, memb)
+
+    # ==============================================
+    # check new training data and validation data
+    # ==============================================
+    root_path = r'F:\TrainingandEvaluation\evaluation'
+    raw_niigz_paths = glob.glob(os.path.join(root_path, 'RawMemb', '*.nii.gz'))
+    for niigz in raw_niigz_paths:
+        embryo_name, tp = os.path.basename(niigz).split('.')[0].split('_')[:2]
+        print(embryo_name, tp)
+        raw_memb_shape = nib_load(niigz).shape
+        nuc_path = os.path.join(root_path, 'RawNuc', '{}_{}_rawNuc.nii.gz'.format(embryo_name, tp))
+        raw_nuc_shape = nib_load(nuc_path).shape
+        seg_cell_path = os.path.join(root_path, 'SegCell', '{}_{}_segCell.nii.gz'.format(embryo_name, tp))
+        seg_cell_shape = nib_load(seg_cell_path).shape
+        seg_nuc_path = os.path.join(root_path, 'SegNuc', '{}_{}_segNuc.nii.gz'.format(embryo_name, tp))
+        seg_nuc_shape = nib_load(seg_nuc_path).shape
+        assert raw_memb_shape == raw_nuc_shape
+        assert raw_nuc_shape == seg_nuc_shape
+        assert seg_cell_shape == seg_nuc_shape
 
 
 if __name__ == "__main__":
     # resize_the_segcell_niigz()
-    embryo_names = ['191108plc1p1']
-    # '200109plc1p1', '200113plc1p2', '200113plc1p3', '200322plc1p2', '200323plc1p1',
+    embryo_names = ['200109plc1p1']
+    # '191108plc1p1'，'200109plc1p1', '200113plc1p2', '200113plc1p3', '200322plc1p2', '200323plc1p1',
     # '200326plc1p3', '200326plc1p4', '200122plc1lag1ip1', '200122plc1lag1ip2', '200117plc1pop1ip2',
     # '200117plc1pop1ip3']
     tps = [205, 205, 255, 195, 195, 185, 220, 195, 195, 195, 140, 155]
     max_middle_num = 5
     root = r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\MembraneProjectData\GUIData\tem\obj_seperated'
     tiff_map_txt_path = r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\MembraneProjectData\GUIData\tem\tiff\tiffmaptxt'
-    # rename_objs(embryo_names,tps,max_middle_num,root,tiff_map_txt_path)
+    rename_objs(embryo_names,tps,max_middle_num,root,tiff_map_txt_path)
 
     target_root = r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\MembraneProjectData\GUIData\tem\obj_combined'
     combine_objs(embryo_names,tps,max_middle_num,root,target_root)
--- tem.py
+++ tem.py
@@ -7,90 +7,8 @@ from PIL import Image
 import nibabel as nib
 from skimage.transform import resize
 from skimage.exposure import rescale_intensity
-from utils.utils import nib_load, nib_save, P, get_boundary
-
-# src_folder = r"C:\Users\bcc\Desktop\tem\crop\train"
-# dst_folder = r"C:\Users\bcc\Desktop\tem\crom17\train"
-# target_str = "pm17"
-#
-# target_files = glob.glob(os.path.join(src_folder, "**"), recursive=True)
-#
-# target_files = [file for file in target_files if target_str in file]
-#
-# for file in tqdm(target_files, desc="Trans {}".format(src_folder)):
-#     target_file = os.path.join(dst_folder, os.path.basename(os.path.dirname(file)), os.path.basename(file).replace("crop", "crom17"))
-#     if not os.path.isdir(os.path.dirname(target_file)):
-#         os.makedirs(os.path.dirname(target_file))
-#     shutil.copyfile(src=file, dst=target_file)
-
-# ########### Draw 3D images for thesis
-# membrane_file = r"D:\OneDriveBackup\OneDrive - City University of Hong Kong\paper\6_NCommunication\Submission\CShaper Supplementary Data\Segmentation Results\RawData\Sample07\RawMemb\Sample07_020_rawMemb.nii.gz"
-# nucleus_file = r"D:\OneDriveBackup\OneDrive - City University of Hong Kong\paper\6_NCommunication\Submission\CShaper Supplementary Data\Segmentation Results\RawData\Sample07\RawNuc\Sample07_020_rawNuc.nii.gz"
-# save_file = r"D:\OneDriveBackup\OneDrive - City University of Hong Kong\study\Thesis\Tem\TemFigures\200309plc1p1_020_combined.tif"
-# memb = nib_load(membrane_file)
-# nucleus = nib_load(nucleus_file)
-#
-# combined_mask = np.zeros_like(memb, dtype=np.uint8)
-# v_min, v_max = np.percentile(memb, (0.2, 99.9))
-# memb = rescale_intensity(memb, in_range=(v_min, v_max)) * 255
-# v_min, v_max = np.percentile(nucleus, (0.2, 99.9))
-# nucleus = rescale_intensity(nucleus, in_range=(v_min, v_max)) * 255
-#
-# combined_mask[nucleus > 5] = 1
-# combined_mask[memb > 35] = 2
-#
-# tif_imgs = []
-# num_slices = combined_mask.shape[-1]
-# for i_slice in range(num_slices):
-#     tif_img = Image.fromarray(combined_mask[..., i_slice], mode="P")
-#     tif_img.putpalette(P)
-#     tif_imgs.append(tif_img)
-#
-# tif_imgs[0].save(save_file, save_all=True, append_images=tif_imgs[1:])
-
-# ################# Draw videos
-'''
-src_folder = r"D:\tem\SegCellTimeCombinedLabelUnified"
-dst_folder = r"D:\tem\3DLapseEmbryo\tif"
-
-seg_files = sorted(glob.glob(os.path.join(src_folder, "*.nii.gz")))
-label_file = os.path.join(os.path.dirname(dst_folder), "label.txt")
-
-for idx, seg_file in enumerate(tqdm(seg_files, desc="Svaing to {}".format(dst_folder))):
-    base_name = os.path.basename(seg_file).split(".")[0]
-
-    seg0 = nib_load(seg_file)
-    seg = seg0 % 255
-    reduce_mask = np.logical_and(seg0!=0, seg==0)
-    seg[reduce_mask] = 255
-    seg = seg.astype(np.uint8)
-    origin_shape = seg.shape
-    out_size = [int(x / 1.5) for x in origin_shape]
-
-    seg = resize(image=seg, output_shape=out_size, preserve_range=True, order=0).astype(np.uint8)
-
-
-    seg = seg[..., :(out_size[-1] // 2)]
-    tif_imgs = []
-    num_slices = seg.shape[-1]
-    for i_slice in range(num_slices):
-        tif_img = Image.fromarray(seg[..., i_slice], mode="P")
-        tif_img.putpalette(P)
-        tif_imgs.append(tif_img)
-    save_file = os.path.join(dst_folder, "_".join([base_name, "render.tif"]))
-    if os.path.isfile(save_file):
-        os.remove(save_file)
-
-    label_num = np.unique(seg).tolist()[-1]
-    if idx == 0:
-        with open(label_file, "w") as f:
-            f.write("{}\n".format(label_num))
-    else:
-        with open(label_file, "a") as f:
-            f.write("{}\n".format(label_num))
-
-    tif_imgs[0].save(save_file, save_all=True, append_images=tif_imgs[1:])
-'''
+from utils.utils import nib_save, get_boundary
+from seperate_process import set_up_new_training_and_evaluation_data
 
 # =================================================
 # ============= write words
@@ -111,35 +29,6 @@ for idx, seg_file in enumerate(tqdm(seg_files, desc="Svaing to {}".format(dst_fo
 #     num_labels = len(seg) - 1
 #     print(f"TP:  {tp}, cells: {num_labels}")
 
-# =================================================
-#  Set 3DMMS evaluation dataset for training CShaper
-# =================================================
-# tps = [24, 34, 44, 54, 64, 74]
-# seg_folder = r"D:\OneDriveBackup\OneDrive - City University of Hong Kong\paper\3_paper for DTwatershed\3DMMS\Evaluation\GroundTruth"
-# dst_folder = r"D:\OneDriveBackup\OneDrive - City University of Hong Kong\study\Thesis\DefenseFigures\3DMMSDataset\170704plc1p1"
-# embryo_name = "170704plc1p1"
-#
-# seg_files = glob.glob(os.path.join(seg_fo"*.nii"))
-# # seg_files.sort()lder,
-#
-# for idx, seg_file in enumerate(seg_files):
-#     seg = nib_load(seg_file)
-#     memb = get_boundary(seg).astype(np.uint8)
-#     save_name_cell = os.path.join(dst_folder, "{}_{}_segCell.nii.gz".format(embryo_name, str(tps[idx]).zfill(3)))
-#     save_name_memb = os.path.join(dst_folder, "{}_{}_segMemb.nii.gz".format(embryo_name, str(tps[idx]).zfill(3)))
-#     nib_save(save_name_cell, seg)
-#     nib_save(save_name_memb, memb)
 
-# ===============================
-# Resave data
-# ===============================
-import pandas as pd
-from utils.data_structure import read_new_cd
-cd_file = r"C:\Users\bcc\Desktop\CD200113plc1p2.csv"
-nuc_pd = read_new_cd(cd_file)
-nuc_pd[["none",	"global", "local", "blot", "cross", "size", "gweight"]] = ""
-nuc_pd = nuc_pd.rename(columns={"Cell & Time":"cellTime"})
-nuc_pd = nuc_pd[['cellTime', 'cell', 'time', 'none', 'global', 'local',
-       'blot', 'cross', 'z', 'x', 'y', 'size', 'gweight']]
-nuc_pd.to_csv(cd_file, index=False)
-print("test")
+set_up_new_training_and_evaluation_data()
+
